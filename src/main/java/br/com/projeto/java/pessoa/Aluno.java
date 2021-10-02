4370c1845acb5028c6a6412e86813c1b66f75a41
Almir Junior de Lima Santos <ajr_almir@hotmail.com>
2021-10-02 13:57:58 -0300

    Lista com alunos, professores e livros

--- src/main/java/br/com/projeto/java/pessoa/Aluno.java
+++ src/main/java/br/com/projeto/java/pessoa/Aluno.java
@@ -1,6 +1,12 @@
 package br.com.projeto.java.pessoa;
 
+import java.time.LocalDate;
+
 public class Aluno extends Pessoa {
     public static final int QTD_LIVROS = 3;
     public static final int DIAS_UTEIS = 10;
+
+    public Aluno(String nome, String matricula, String email, LocalDate dataEmprestimo, LocalDate dataDevolucao, LocalDate dataBloqueio, int qtdDiasBloqueados, int qtdLivrosEmprestados) {
+        super(nome, matricula, email, dataEmprestimo, dataDevolucao, dataBloqueio, qtdDiasBloqueados, qtdLivrosEmprestados);
+    }
 }
--- src/main/java/br/com/projeto/java/pessoa/Professor.java
+++ src/main/java/br/com/projeto/java/pessoa/Professor.java
@@ -1,6 +1,12 @@
 package br.com.projeto.java.pessoa;
 
+import java.time.LocalDate;
+
 public class Professor extends Pessoa {
     public static final int QTD_LIVROS = 5;
     public static final int DIAS_UTEIS = 20;
+
+    public Professor(String nome, String matricula, String email, LocalDate dataEmprestimo, LocalDate dataDevolucao, LocalDate dataBloqueio, int qtdDiasBloqueados, int qtdLivrosEmprestados) {
+        super(nome, matricula, email, dataEmprestimo, dataDevolucao, dataBloqueio, qtdDiasBloqueados, qtdLivrosEmprestados);
+    }
 }
