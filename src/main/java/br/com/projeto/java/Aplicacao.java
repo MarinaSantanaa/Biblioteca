d162a71b32d0d36a282e18d65593f87afa0df6eb
Marina <mari.santanaa@hotmail.com>
2021-10-02 11:36:24 -0300

    Classe pessoa alterada

--- src/main/java/br/com/projeto/java/Aplicacao.java
+++ src/main/java/br/com/projeto/java/Aplicacao.java
@@ -3,6 +3,5 @@ package br.com.projeto.java;
 public class Aplicacao {
     public static void main(String[] args) {
 
-        String testeCommit = "teste";
     }
 }
--- src/main/java/br/com/projeto/java/pessoa/Pessoa.java
+++ src/main/java/br/com/projeto/java/pessoa/Pessoa.java
@@ -2,6 +2,8 @@ package br.com.projeto.java.pessoa;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,4 +14,6 @@ public abstract class Pessoa {
     private String nome;
     private String matricula;
     private String email;
+    private LocalDate dataBloqueio;
+    private int qtdDiasBloqueados;
 }
