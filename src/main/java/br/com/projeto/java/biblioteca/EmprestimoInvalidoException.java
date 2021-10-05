7094df05ef695da77e9570c62174c2d2bc746cb3
Alexandre <ars1804@yahoo.com.br>
2021-10-05 16:45:54 -0300

    .

--- src/main/java/br/com/projeto/java/biblioteca/EmprestimoInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package br.com.projeto.java.biblioteca;
-
-public class EmprestimoInvalidoException extends Exception{
-    public EmprestimoInvalidoException(){
-        super("Não é possivel realizar o emprestimo no momento.");
-    }
-}
